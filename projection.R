111de94139cf77d681cab9e9baebf514b4af8ab3
Alex Miller <alex.k.miller@gmail.com>
2020-02-26 14:33:57 -0500

    January 2020 data

--- projection.R
+++ projection.R
@@ -21,12 +21,15 @@ dat_september = fread("data/MVA_Electric_and_Hybrid_Vehicle_Registrations_by_Cou
 setnames(dat_september,"Count","2019-09-01")
 dat_december = fread("data/MVA_Electric_and_Hybrid_Vehicle_Registrations_by_County_as_of_December_2019.csv")
 setnames(dat_december,"Count","2019-12-01")
+dat_jan20 = fread("data/MVA_Electric_and_Hybrid_Vehicle_Registrations_by_County_as_of_January_2020.csv")
+setnames(dat_jan20,"Count","2020-01-01")
 dat_reg = merge(dat_march,dat_april)
 dat_reg = merge(dat_reg,dat_may)
 dat_reg = merge(dat_reg,dat_june)
 dat_reg = merge(dat_reg,dat_august)
 dat_reg = merge(dat_reg,dat_september)
 dat_reg = merge(dat_reg,dat_december)
+dat_reg = merge(dat_reg,dat_jan20)
 
 dat_melt = melt(dat_reg,id.vars=c("Fuel_Category","County"),variable.name="Date")
 dat_cast = dcast(dat_melt,County+Date~Fuel_Category)
@@ -49,20 +52,21 @@ missing_months = data.frame(
 )
 dat_tab = rbindlist(list(dat_tab,missing_months), fill=T)
 dat_tab = dat_tab[order(dat_tab$Date)]
-dat_tab$PHEV = na.approx(dat_tab$PHEV,na.rm=F)
-dat_tab$BEV = na.approx(dat_tab$BEV,na.rm=F)
-# First difference, as MVA data is in total
-dat_tab$BEV = c(NA,diff(dat_tab$BEV))
-dat_tab$PHEV = c(NA,diff(dat_tab$PHEV))
 
 dat_alliance = fread("data/alliance_dat.csv")
 dat_alliance$Date = as.Date(dat_alliance$Date,format="%m/%d/%y")
+dat_alliance$BEV = cumsum(dat_alliance$BEV)
+dat_alliance$PHEV = cumsum(dat_alliance$PHEV)
 
 # Combine all our sources
 dat = rbindlist(list(dat_alliance,dat_tab),fill=T)
 dat = dat[order(dat$Date)]
-dat$BEV = na.approx(dat$BEV)
-dat$PHEV = na.approx(dat$PHEV)
+dat$BEV = na.spline(dat$BEV)
+dat$PHEV = na.spline(dat$PHEV)
+
+dat$BEV = c(NA,diff(dat$BEV))
+dat$PHEV = c(NA,diff(dat$PHEV))
+dat = subset(dat,!is.na(BEV))
 
 dat$ZEV = dat$BEV + dat$PHEV
 
@@ -78,7 +82,7 @@ zev_burn <- SuggestBurn(0.1, zev_bsts.model)
 
 # Predict until December 2024
 zev_p <- predict.bsts(zev_bsts.model, horizon = 60, burn = zev_burn, quantiles = c(.025, .975))
-zev_p.ts = ts(rep(NA,60),frequency=12,start=c(2020,1))
+zev_p.ts = ts(rep(NA,60),frequency=12,start=c(2020,2))
 ### Actual versus predicted
 zev_d2 <- data.frame(
   # fitted values and predictions
@@ -93,7 +97,7 @@ names(zev_d2) <- c("Fitted", "Actual", "Date")
 zev_posterior.interval <- cbind.data.frame(
   as.numeric(zev_p$interval[1,]),
   as.numeric(zev_p$interval[2,]), 
-  subset(zev_d2, Date>as.Date("2019-12-01"))$Date)
+  subset(zev_d2, Date>as.Date("2020-01-01"))$Date)
 names(zev_posterior.interval) <- c("LL", "UL", "Date")
 
 ### Join intervals to the forecast
@@ -114,12 +118,14 @@ d4$LL_sum = cumsum(d4$actual_plus_LL)
 d4$UL_sum = cumsum(d4$actual_plus_UL)
 
 sum.p = ggplot(data=d4, aes(x=Date)) +
-  geom_line(data=subset(d4,Date<as.Date("2020-01-01")),aes(y=estimated_sum, colour = "Actual"), size=1.2) +
-  geom_line(data=subset(d4,Date>=as.Date("2020-01-01")),aes(y=estimated_sum, colour = "Estimate"), size=1.2, linetype=1) +
+  geom_line(data=subset(d4,Date<as.Date("2020-02-01")),aes(y=estimated_sum, colour = "Actual"), size=1.2) +
+  geom_line(data=subset(d4,Date>=as.Date("2020-02-01")),aes(y=estimated_sum, colour = "Estimate"), size=1.2, linetype=1) +
   theme_bw() + theme(legend.title = element_blank()) + ylab("") + xlab("") +
-  geom_vline(xintercept=as.numeric(as.Date("2020-01-01")), linetype=2) + 
-  geom_ribbon(data=subset(d4,Date>=as.Date("2020-01-01")),aes(ymin=LL_sum, ymax=UL_sum, fill="95% conf."), alpha=0.5) +
+  geom_vline(xintercept=as.numeric(as.Date("2020-02-01")), linetype=2) + 
+  geom_ribbon(data=subset(d4,Date>=as.Date("2020-02-01")),aes(ymin=LL_sum, ymax=UL_sum, fill="95% conf."), alpha=0.5) +
   scale_fill_manual("", values=c("95% conf."="grey")) +
+  scale_y_continuous(labels=scales::comma) +
+  expand_limits(y=300000) +
   theme(axis.text.x=element_text(angle = -90, hjust = 0))+
-  labs(title="Cumulative Maryland ZEV sales from 2011-2019, forecast to 2025")
+  labs(title="Cumulative Maryland ZEV sales from 2011- Jan. 2020, forecast to 2025")
 ggsave("output/cumulative_projection.png", sum.p, width=10, height=5)
